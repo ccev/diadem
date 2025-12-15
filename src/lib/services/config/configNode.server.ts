8257c50ab52d8b4e0724d2a6911c2d39dfa0b855
ComplementaryPogo <complementary@metrapogo.org>
2025-12-15 04:31:11 -0600

    Fix drizzle-kit imports to use local path

--- src/lib/services/config/configNode.server.ts
+++ src/lib/services/config/configNode.server.ts
@@ -1,7 +1,7 @@
 import * as fs from 'node:fs';
-import type { Config } from '@/lib/services/config/config';
+import type { Config } from './configTypes';
 import { parse } from 'toml';
-import { getDbUri } from '@/lib/services/config/dbUri.server';
+import { getDbUri } from './dbUri.server';
 
 const configFile = fs.readFileSync("./src/lib/server/config.toml", "utf8")
 const config: Config = parse(configFile);
