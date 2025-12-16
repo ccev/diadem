e7744c3cb5ef7d53f7681fc45544fab7158d6677
ComplementaryPogo <complementary@metrapogo.org>
2025-12-15 20:25:06 -0600

    Don't try to load client config while we're a server

--- src/lib/utils/getMapPath.ts
+++ src/lib/utils/getMapPath.ts
@@ -1,7 +1,15 @@
 import { getConfig } from "@/lib/services/config/config";
+import { browser } from "$app/environment";
 
 export function getMapPath(suffix: string = "") {
-	if (getConfig().general.customHome) {
+	// On server-side, the client config store isn't populated
+	// Return safe default - customHome redirect is handled client-side
+	if (!browser) {
+		return suffix ? suffix : "/";
+	}
+
+	const config = getConfig();
+	if (config?.general?.customHome) {
 		let path = "/map"
 		if (suffix) path += suffix
 		return path
