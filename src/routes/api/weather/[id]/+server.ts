34d3dc712ad5b79a6f9b8e1a7a76a450adeb27e9
Malte <42342921+ccev@users.noreply.github.com>
2026-01-19 14:03:05 +0100

    fix: use features enum for weather permissions

--- src/routes/api/weather/[id]/+server.ts
+++ src/routes/api/weather/[id]/+server.ts
@@ -2,12 +2,13 @@ import { error, json } from "@sveltejs/kit";
 import { query } from "@/lib/server/db/external/internalQuery";
 import { getLogger } from "@/lib/server/logging";
 import { hasFeatureAnywhereServer } from "@/lib/server/auth/checkIfAuthed";
+import { Features } from '@/lib/utils/features';
 
 const log = getLogger("mapobjects");
 
 export async function GET({ params, locals }) {
 	const start = performance.now();
-	if (!hasFeatureAnywhereServer(locals.perms, "weather", locals.user)) error(401);
+	if (!hasFeatureAnywhereServer(locals.perms, Features.WEATHER, locals.user)) error(401);
 	const permCheckTime = performance.now();
 
 	const result = await query("SELECT * FROM weather " + "WHERE id = ?", [params.id]);
