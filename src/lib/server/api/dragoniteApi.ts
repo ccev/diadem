13214db10dffe9320ba602891f6c34c078088064
Malte <42342921+ccev@users.noreply.github.com>
2026-02-05 22:46:30 +0100

    chore: add scout error logs

--- src/lib/server/api/dragoniteApi.ts
+++ src/lib/server/api/dragoniteApi.ts
@@ -1,18 +1,21 @@
-import { getServerConfig } from '@/lib/services/config/config.server';
+import { getServerConfig } from "@/lib/services/config/config.server";
+import { getLogger } from "@/lib/utils/logger";
+
+const log = getLogger("dragonite");
 
 function getHeaders() {
-	const headers: HeadersInit = {}
+	const headers: HeadersInit = {};
 	if (getServerConfig().dragonite.secret) {
-		headers['X-Dragonite-Admin-Secret'] = getServerConfig().dragonite.secret ?? ""
+		headers["X-Dragonite-Admin-Secret"] = getServerConfig().dragonite.secret ?? "";
 	}
 	if (getServerConfig().dragonite.basicAuth) {
-		headers['Authorization'] = `Basic ${btoa(getServerConfig().dragonite.basicAuth ?? '')}`;
+		headers["Authorization"] = `Basic ${btoa(getServerConfig().dragonite.basicAuth ?? "")}`;
 	}
-	return headers
+	return headers;
 }
 
 export async function addScoutEntries(username: string, locations: number[][]) {
-	const url = new URL("scout/v2", getServerConfig().dragonite.url)
+	const url = new URL("scout/v2", getServerConfig().dragonite.url);
 
 	const requestData = {
 		username,
@@ -23,19 +26,33 @@ export async function addScoutEntries(username: string, locations: number[][]) {
 			pokemon: true,
 			gmf: true
 		}
+	};
+
+	const response = await fetch(url, {
+		headers: getHeaders(),
+		method: "POST",
+		body: JSON.stringify(requestData)
+	});
+
+	if (!response.ok) {
+		log.error("Error while queueing scout entry [%d] %s", response.status, await response.text());
 	}
 
-	const response = await fetch(url, { headers: getHeaders(), method: "POST", body: JSON.stringify(requestData) })
-
-	return response.ok
+	return response.ok;
 }
 
 export async function getScoutQueue(): Promise<number | undefined> {
-	const url = new URL("scout/queue", getServerConfig().dragonite.url)
-	const response = await fetch(url, { headers: getHeaders() })
+	const url = new URL("scout/queue", getServerConfig().dragonite.url);
+	const response = await fetch(url, { headers: getHeaders() });
 
 	if (response.ok) {
-		const data = await response.json()
-		return data?.queue ?? undefined
+		const data = await response.json();
+		return data?.queue ?? undefined;
+	} else {
+		log.error(
+			"Error while getting scout queue size [%d] %s",
+			response.status,
+			await response.text()
+		);
 	}
 }
