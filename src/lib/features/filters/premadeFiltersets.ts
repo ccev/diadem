1d6d72897b8471f179c6a2c0348c0dbc9b2ec0e5
Malte <42342921+ccev@users.noreply.github.com>
2026-04-13 01:28:04 +0200

    remove legendary bird premade

--- src/lib/features/filters/premadeFiltersets.ts
+++ src/lib/features/filters/premadeFiltersets.ts
@@ -84,18 +84,6 @@ export const premadeFiltersets: { [key in FilterCategory]?: FiltersetPokemon[] }
 		filterset<FiltersetPokemon>({
 			uicon: {
 				category: IconCategory.POKEMON,
-				params: { pokemon_id: 144, form: 0 }
-			},
-			title: "filter_template_legendary_birds",
-			pokemon: [
-				{ pokemon_id: 144, form: 0 },
-				{ pokemon_id: 145, form: 0 },
-				{ pokemon_id: 146, form: 0 }
-			]
-		}),
-		filterset<FiltersetPokemon>({
-			uicon: {
-				category: IconCategory.POKEMON,
 				params: { pokemon_id: 480, form: 0 }
 			},
 			title: "filter_template_sea_trio",
