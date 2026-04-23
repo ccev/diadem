ee2c7310eac2efe0252d12130fd562445260ecd7
Malte <42342921+ccev@users.noreply.github.com>
2026-04-23 02:17:44 +0200

    fix: allow more paths in robots.txt

--- src/lib/utils/disallowedPaths.ts
+++ src/lib/utils/disallowedPaths.ts
@@ -3,13 +3,10 @@ import { getConfig } from "@/lib/services/config/config";
 export function getDisallowedPaths() {
 	return [
 		...(getConfig().general.disallowedPaths ?? []),
-		"/api",
 		"/map",
 		"/login",
 		"/logout",
 		"/coverage",
-		"/assets",
-		"/a",
 		"/filter"
 	];
 }
